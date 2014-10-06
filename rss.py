28b05222b0b27d1b131c118fffa89fdc8893f7f9
eytan <eytan@chartbeat.com>
2014-10-05 22:54:50 -0700

    parellalize fetch

--- rss.py
+++ rss.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from datetime import timedelta
 from flask import Flask
-import requests
+import grequests
 from werkzeug.contrib.atom import AtomFeed
 
 app = Flask(__name__)
@@ -28,14 +28,13 @@ def _get_url(date):
     month = MONTHS[date.month - 1]
     return TEMPLATE % (date.day, month, date.year)
 
-def _get_item(date):
+def _get_item(date, content):
     """
     @param date: date
     @return: dict
     """
     url = _get_url(date)
-    content = requests.get(url)
-    soup = BeautifulSoup(content.text)
+    soup = BeautifulSoup(content)
     headers = soup.find('div', {'class': 'entry-content'}).find_all('h4')
     month = MONTHS[date.month - 1]
     title = 'Aksam Postasi - %s %s %s' % (date.day, month, date.year)
@@ -62,9 +61,11 @@ def get_feed(num_days, start_date=None):
                     feed_url='http://www.diken.com.tr/',
                     url='http://www.diken.com.tr/',
                     )
+    dates = [start_date - timedelta(days=i) for i in range(num_days)]
+    rs = (grequests.get(_get_url(d)) for d in dates)
+    contents = [res.content for res in grequests.map(rs)]
     for i in range(num_days):
-        date = start_date - timedelta(days=i)
-        item = _get_item(date)
+        item = _get_item(dates[i], contents[i])
         content = item['content']; del(item['content'])
         title = item['title']; del(item['title'])
         feed.add(title, content, **item)
@@ -79,4 +80,4 @@ if __name__ == '__main__':
             host='0.0.0.0',
             debug=True,
             )
-    #get_feed(3)
+    # get_feed(3)
